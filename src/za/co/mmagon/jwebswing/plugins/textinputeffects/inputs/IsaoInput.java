568c3e227ed6cddd56de87d41950a016a237fe7c
Marc Magon <ged_marc@hotmail.com>
2017-12-19 01:33:13 +0200

    Text Effects First commit, test classes look good

--- src/za/co/mmagon/jwebswing/plugins/textinputeffects/inputs/IsaoInput.java
+++ src/za/co/mmagon/jwebswing/plugins/textinputeffects/inputs/IsaoInput.java
@@ -3,10 +3,12 @@ package za.co.mmagon.jwebswing.plugins.textinputeffects.inputs;
 import za.co.mmagon.jwebswing.base.html.Input;
 import za.co.mmagon.jwebswing.base.html.Label;
 import za.co.mmagon.jwebswing.base.html.Span;
+import za.co.mmagon.jwebswing.plugins.ComponentInformation;
 
 /**
  * Creates a haruki input style
  */
+@ComponentInformation(name = "Isao Input Effect", description = "A very nice effect for inputs", url = "https://tympanus.net/Development/TextInputEffects/index2.html")
 public class IsaoInput extends InputEffect
 {
 	/**
--- src/za/co/mmagon/jwebswing/plugins/textinputeffects/inputs/SaeInput.java
+++ src/za/co/mmagon/jwebswing/plugins/textinputeffects/inputs/SaeInput.java
@@ -4,10 +4,12 @@ import za.co.mmagon.jwebswing.base.html.Input;
 import za.co.mmagon.jwebswing.base.html.Italic;
 import za.co.mmagon.jwebswing.base.html.Label;
 import za.co.mmagon.jwebswing.base.html.Span;
+import za.co.mmagon.jwebswing.plugins.ComponentInformation;
 
 /**
  * Creates a haruki input style
  */
+@ComponentInformation(name = "Sae Input Effect", description = "A very nice effect for inputs", url = "https://tympanus.net/Development/TextInputEffects/index2.html")
 public class SaeInput extends InputEffect
 {
 
