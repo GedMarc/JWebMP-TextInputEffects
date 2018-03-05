b23e4362a19853283ce23a964c1260ede2ab7b43
Marc Magon <ged_marc@hotmail.com>
2018-03-05 08:44:47 +0200

    Major Update - Angular Directive Service Location Bootstrap 4 Final - Up To BSForm Updated

--- test/za/co/mmagon/jwebswing/plugins/textinputeffects/inputs/RuriInputTest.java
+++ test/za/co/mmagon/jwebswing/plugins/textinputeffects/inputs/RuriInputTest.java
@@ -1,9 +1,9 @@
 package za.co.mmagon.jwebswing.plugins.textinputeffects.inputs;
 
 import org.junit.jupiter.api.Test;
-import za.co.mmagon.jwebswing.base.html.InputTextType;
 import za.co.mmagon.jwebswing.base.html.Label;
 import za.co.mmagon.jwebswing.base.html.Span;
+import za.co.mmagon.jwebswing.base.html.inputs.InputTextType;
 import za.co.mmagon.jwebswing.plugins.textinputeffects.inputs.set2.RuriSet2Input;
 
 class RuriInputTest
