81a00b6f9dd1b067b0b0c515620ef4b39f316dd4
GedMarc <ged_marc@hotmail.com>
2019-08-19 00:45:03 +0200

    Pro Sidebar and Updates

--- src/main/java/com/jwebmp/plugins/textinputeffects/inputs/set1/KaedeInput.java
+++ src/main/java/com/jwebmp/plugins/textinputeffects/inputs/set1/KaedeInput.java
@@ -29,7 +29,7 @@ import com.jwebmp.core.plugins.ComponentInformation;
 		description = "A very nice effect for inputs",
 		url = "https://tympanus.net/Development/TextInputEffects/index2.html")
 public class KaedeInput
-		extends Set1InputEffect
+		extends Set1InputEffect<KaedeInput>
 {
 	/**
 	 * Creates a new haruki styled input
