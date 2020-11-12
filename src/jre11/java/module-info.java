46a539796fd05531b055d9019a9c4ade69b597c6
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:40:04 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -7,7 +7,7 @@ module com.jwebmp.plugins.textinputeffects {
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
 
-	requires java.validation;
+	requires jakarta.validation;
 	requires java.logging;
 	requires com.guicedee.guicedinjection;
 
