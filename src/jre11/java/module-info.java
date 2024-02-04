cbee2fc713cb2e312e6348edc9f2a4d65d30125f
Marc Magon <marc.magon@gmail.com>
2024-02-04 23:43:34 +0200

    Updates for version 2

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -7,7 +7,7 @@ module com.jwebmp.plugins.textinputeffects {
 	exports com.jwebmp.plugins.textinputeffects.inputs.set2;
 
 	requires com.jwebmp.core;
-	requires com.guicedee.logmaster;
+	
 
 	requires jakarta.validation;
 	requires java.logging;
